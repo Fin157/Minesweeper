e82bd9f5f62f24959d405e857fbd7b56f1d26f28
Fin157 <444stuart@gmail.com>
2023-03-14 21:03:33 +0100

    Finalized the tile system classes.

---
+++ Gameplay/TileSystem/ClearTile.cs
@@ -0,0 +1,34 @@
+﻿namespace Minesweeper.Gameplay.TileSystem;
+
+/// <summary>
+/// A class representing a tile where no mine is hidden
+/// </summary>
+internal class ClearTile : Tile
+{
+    /// <summary>
+    /// Returns how many mines this tile neighbours. 
+    /// Returns the real value if the tile has already been uncovered, otherwise returns -1.
+    /// </summary>
+    public int MinesAround
+    {
+        get
+        {
+            return isUncovered ? minesAround : -1;
+        }
+    }
+
+    /// <summary>
+    /// Stores how many mines this tile neighbours
+    /// </summary>
+    private readonly int minesAround;
+
+    public ClearTile(int x, int y, bool isDarker, int minesAround) : base(x, y, isDarker)
+    {
+        this.minesAround = minesAround;
+    }
+
+    protected override void OnUncover()
+    {
+        // Do nothing, this tile is inert
+    }
+}
---
+++ Gameplay/TileSystem/Tile.cs
@@ -0,0 +1,69 @@
+﻿using Minesweeper.Rendering;
+
+namespace Minesweeper.Gameplay.TileSystem;
+
+/// <summary>
+/// A base class for any tile on the map
+/// </summary>
+internal abstract class Tile
+{
+    /// <summary>
+    /// Where this tile is on the map
+    /// </summary>
+    public Position Position { get; protected set; }
+    /// <summary>
+    /// If this tile has a mark on it or not
+    /// </summary>
+    public bool IsMarked
+    {
+        get
+        {
+            return isMarked;
+        }
+        set
+        {
+            // Prevent ouside code from marking an uncovered tile
+            if (isUncovered)
+                return;
+
+            isMarked = value;
+        }
+    }
+    /// <summary>
+    /// If this tile has been already dug up or not
+    /// </summary>
+    public bool IsUncovered
+    {
+        get
+        {
+            return isUncovered;
+        }
+        set
+        {
+            isUncovered = value;
+            if (value) // Execute only if the new value is "true"
+                OnUncover();
+        }
+    }
+    /// <summary>
+    /// Returns a two chars long string to be rendered as the texture of this tile
+    /// </summary>
+    public RenderLine Texture { get => renderer.Texture; }
+
+    protected bool isUncovered;
+    protected bool isMarked;
+    protected TileTexture renderer;
+
+    public Tile(int x, int y, bool isDarker)
+    {
+        IsMarked = false;
+        IsUncovered = false;
+        Position = new(x, y);
+        renderer = new(this, isDarker);
+    }
+
+    /// <summary>
+    /// Gets called when this tile gets dug up
+    /// </summary>
+    protected abstract void OnUncover();
+}
